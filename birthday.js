99b271e48c630c35046a0d765eae4cb50b3433fa
webdevil80 <69701204+webdevil80@users.noreply.github.com>
2025-05-23 22:14:17 +0530

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -1,44 +1,48 @@
 let countdownDiv;
-let force;
 let firework = [];
+let force;
 let timerEnded = false;
+
+let now = new Date();
+let targetDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 22, 15, 0); // 10:15 PM today
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  force = createVector(0, 0.2);
   countdownDiv = select('#timer');
-  setInterval(updateClock, 1000); // update every second
+  frameRate(30);
 }
 
 function draw() {
-  background(0);
-}
+  background(0, 25);
 
-function updateClock() {
-  let now = new Date();
-  let hours = nf(now.getHours(), 2);
-  let minutes = nf(now.getMinutes(), 2);
-  let seconds = nf(now.getSeconds(), 2);
-  countdownDiv.html(`Current Time: ${hours}:${minutes}:${seconds}`);
-}
-
-function draw() {
   if (!timerEnded) {
-    background(0);
+    let now = new Date();
+    if (now >= targetDate) {
+      timerEnded = true;
+      countdownDiv.hide();
+      location.reload(); // Refresh to restart animation
+      return;
+    }
+
+    // Show countdown
+    let diff = targetDate - now;
+    let hours = nf(floor((diff / (1000 * 60 * 60)) % 24), 2);
+    let minutes = nf(floor((diff / (1000 * 60)) % 60), 2);
+    let seconds = nf(floor((diff / 1000) % 60), 2);
+    countdownDiv.html(`Countdown to birthday:<br>${hours}:${minutes}:${seconds}`);
+
     return;
   }
 
-  countdownDiv.hide(); // Hide timer once animation starts
-
-  colorMode(RGB);
-  background(0, 25);
-
+  // After countdown ends
   if (frameCount % 20 === 0) {
     firework.push(new fireworks());
   }
 
   for (let i = 0; i < firework.length; i++) {
     firework[i].update();
-    firework[i].show();      
+    firework[i].show();
   }
 
   textSize(30);
@@ -67,7 +71,7 @@ function particle(x, y, col, firework) {
 
   this.acc = createVector(0, -4);
 
-  this.show = function() {
+  this.show = function () {
     if (!firework) {
       strokeWeight(2);
       stroke(this.col, 255, 255, this.opacity);
@@ -78,11 +82,11 @@ function particle(x, y, col, firework) {
     point(this.pos.x, this.pos.y);
   }
 
-  this.applyForce = function(force) {
+  this.applyForce = function (force) {
     this.acc.add(force);
   }
 
-  this.update = function() {
+  this.update = function () {
     if (!firework) {
       this.vel.mult(0.99);
       this.opacity -= 7;
@@ -98,7 +102,7 @@ function fireworks() {
   this.explode = false;
   this.particles = [];
 
-  this.update = function() {
+  this.update = function () {
     if (!this.explode) {
       this.firework.applyForce(force);
       this.firework.show();
@@ -120,7 +124,7 @@ function fireworks() {
     }
   }
 
-  this.show = function() {
+  this.show = function () {
     colorMode(HSB);
     if (!this.explode) {
       this.firework.update();
@@ -130,7 +134,7 @@ function fireworks() {
     }
   }
 
-  this.explore = function() {
+  this.explore = function () {
     for (let i = 0; i < 150; i++) {
       this.particles.push(new particle(this.firework.pos.x, this.firework.pos.y, random(255), false));
     }
